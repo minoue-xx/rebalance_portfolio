23678dee0936e10dc8471e126b6fac09fe83ea4c
minoue <minoue@mathworks.com>
2019-11-05 03:47:38 +0900

    deleted makeTable, replacing with makeEmptyTable and fillTableWithData

--- makeTable.js
+++ makeTable.js
@@ -1,6 +1,20 @@
-function makeTable(container, rowdata) {
-
+function fillTableWithData(container, rowdata) {
     rowdata = rowdata.filter(word => word.length > 1);
+
+    let $tblrows = $("#tblTickers tbody tr");
+    $tblrows.each(function (index) {
+        var $tblrow = $(this);
+
+        cells = rowdata[index].split(",");
+        cells = cells.filter(word => word.length > 1);
+
+        $tblrow.find('.ticker').val(cells[0]);
+        $tblrow.find('.qty').val(cells[1]);
+        $tblrow.find('.target').val(cells[2]);
+    });
+}
+
+function makeEmptyTable(container, nBodyRows) {
 
     var table = $("<table/>");
     table.attr('id', 'tblTickers');
@@ -16,104 +30,23 @@ function makeTable(container, rowdata) {
     table.append(thead);
 
     var tbody = $("<tbody/>");
-    $.each(rowdata, function (rowIndex, r) {
+
+    let classes = ["ticker", "qty", "price", "subtot", "actual", "target", "diff"];
+
+    for (var i = 0; i < nBodyRows; i++) {
         var row = $("<tr/>");
-        var cells = rowdata[rowIndex].split(",");
-
-        //let classes = ["ticker", "qty", "price", "subtot", "actual", "target", "diff"];
-        let classes1 = ["ticker", "qty", "price"];
-        cells = cells.filter(word => word.length > 1);
-        $.each(classes1, function (colIndex) {
+        $.each(classes, function (colIndex) {
             var col = $("<td/>");
             var input = $("<input/>");
             input.attr('type', 'text');
-            input.attr('value', cells[colIndex]);
-            input.attr('class', classes1[colIndex]);
-            input.attr('name', classes1[colIndex]);
-            col.append(input);
-            row.append(col);
-        });
-
-        let classes2 = ["subtot", "actual", "target", "diff"];
-        $.each(classes2, function (colIndex) {
-            var col = $("<td/>");
-            var input = $("<input/>");
-            input.attr('type', 'text');
-            input.attr('class', classes2[colIndex]);
-            input.attr('name', classes2[colIndex]);
+            input.attr('class', classes[colIndex]);
+            input.attr('name', classes[colIndex]);
             col.append(input);
             row.append(col);
         });
         tbody.append(row);
-    });
-    table.append(tbody);
+    }
 
-
-    var tfoot = $("<tfoot/>");
-    var row = $("<tr/>");
-    row.append($("<td/>").text(""));
-    row.append($("<td/>").text(""));
-    row.append($("<td/>").text(""));
-
-    var col = $("<td/>");
-    var input = $("<input/>");
-    input.attr('type', 'text');
-    input.attr('value', '');
-
-    input.attr('class', 'grdtot');
-    input.attr('name', 'grdtor');
-
-    col.append(input);
-    row.append(col);
-
-    row.append($("<td/>").text(""));
-    row.append($("<td/>").text(""));
-
-    col = $("<td/>");
-    input = $("<input/>");
-    input.attr('type', 'text');
-    input.attr('value', '');
-
-    input.attr('class', 'grddiff');
-    input.attr('name', 'grddiff');
-
-    col.append(input);
-    row.append(col);
-
-    tfoot.append(row);
-    table.append(tfoot);
-
-    return container.append(table);
-}
-
-function makeEmptyTable(container) {
-
-    var table = $("<table/>");
-    table.attr('id', 'tblTickers');
-    var thead = $("<thead/>");
-    var row = $("<tr/>");
-
-    let names = ["Ticker", "Quantity", "Price", "Sub-Total", "Actual%", "Target%", "Diff%"];
-
-    $.each(names, function (index, value) {
-        row.append($("<td/>").text(value));
-    });
-    thead.append(row);
-    table.append(thead);
-
-    var tbody = $("<tbody/>");
-    var row = $("<tr/>");
-    let classes = ["ticker", "qty", "price", "subtot", "actual", "target", "diff"];
-    $.each(classes, function (colIndex) {
-        var col = $("<td/>");
-        var input = $("<input/>");
-        input.attr('type', 'text');
-        input.attr('class', classes[colIndex]);
-        input.attr('name', classes[colIndex]);
-        col.append(input);
-        row.append(col);
-    });
-    tbody.append(row);
     table.append(tbody);
 
     var tfoot = $("<tfoot/>");
--- onLoad.js
+++ onLoad.js
@@ -7,10 +7,13 @@ $(function () {
                 reader.onload = function (e) {
 
                     var rowdata = e.target.result.split("\n");
+                    rowdata = rowdata.filter(word => word.length > 1);
 
                     let container = $("#dvCSV");
                     container.html('');
-                    makeTable(container, rowdata);
+                    //makeTable(container, rowdata);
+                    makeEmptyTable(container, rowdata.length);
+                    fillTableWithData(container, rowdata);
                     updateQuotes();
                     updateQuotes_updates();
 
@@ -28,8 +31,12 @@ $(function () {
 
         let container = $("#dvCSV");
         container.html('');
-        let rowdata = ["VTI,100", "VEA,100", "VWO,100"];
-        makeTable(container, rowdata);
+        //let rowdata = ["VTI,100", "VEA,100", "VWO,100"];
+        //let rowdata = ["VTI,124", "VYM,98", "VEA,371", "VWO,177", "GLD,48", "AGG,64", "XLRE,90"];
+        let rowdata = ["VTI,124,0.28", "VYM,98,0.13", "VEA,371,0.23", "VWO,177,0.11", "GLD,48,0.10", "AGG,64,0.10", "XLRE,90,0.05"];
+
+        makeEmptyTable(container, rowdata.length);
+        fillTableWithData(container, rowdata);
         updateQuotes();
         updateQuotes_updates();
 
@@ -40,7 +47,7 @@ $(function () {
         let container = $("#dvCSV");
 
         if (container.html().length == 0) {
-            makeEmptyTable(container);
+            makeEmptyTable(container, 2);
             updateQuotes_updates();
         } else {
             appendLastRow(container);
