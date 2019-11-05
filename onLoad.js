64a32b9920757bbc35de6ab676988e03004dd0d9
minoue <minoue@mathworks.com>
2019-11-06 04:59:23 +0900

    add fmincon component

--- onLoad.js
+++ onLoad.js
@@ -90,15 +90,62 @@ $(function () {
         });
 
         let targets = [];
+        var target_pf = [0.275, 0.125, 0.2, 0.1, 0.1, 0.15, 0.05, 0, 0, 0];
         $(".target").each(function (index) {
             targets.push($(this).val());
+            target_pf[index] = $(this).val() / 100;
         });
         $(".target_v").each(function (index) {
             $(this).val(targets[index]);
         });
 
+        // inputs
+        // let cost = 2000;
+
+
+        var price = [155.83, 90.4, 42.78, 42.50, 142.56, 112.93, 39.23, 0, 0, 0];
+        $(".price").each(function (index) {
+            price[index] = parseFloat($(this).val());
+        });
+
+        var position = [12, 20, 40, 15, 6, 18, 12, 0, 0, 0];
+        $(".qty").each(function (index) {
+            position[index] = parseFloat($(this).val());
+        });
+
+        console.log(position);
+        console.log(price);
+        console.log(target_pf);
+
+        // Create Data    
+        var Target_pf = new Float64Array(target_pf);
+        var Price = new Float64Array(price);
+        var Position = new Float64Array(position);
+        var Position2Add = new Float64Array(10);
+        // Move Data to Heap
+        var Target_pfbytes = _arrayToHeap(Target_pf);
+        var Pricebytes = _arrayToHeap(Price);
+        var Positionbytes = _arrayToHeap(Position);
+        var Position2Addbytes = _arrayToHeap(Position2Add);
+        // Run Function
+        _optimizeposition_initialize();
+        _getPosition2Add(Target_pfbytes.byteOffset, Pricebytes.byteOffset, Positionbytes.byteOffset, Position2Addbytes.byteOffset)
+        _optimizeposition_terminate();
+        //  Copy Data from Heap
+        Position2Add = _heapToArray(Position2Addbytes, Position2Add);
+        var position2add = Array.from(Position2Add);
+        // Free Data from Heap
+        _freeArray(Target_pfbytes);
+        _freeArray(Pricebytes);
+        _freeArray(Positionbytes);
+        _freeArray(Position2Addbytes);
+
+        // Display Results
+        console.log("Original Position: " + Position);
+        console.log("Position to add: " + position2add);
+
         // tmp
-        let qty2add = [1, 1, 1];
+        let qty2add = Position2Add;
         $(".qty2add").each(function (index) {
             $(this).val(qty2add[index]);
         });
